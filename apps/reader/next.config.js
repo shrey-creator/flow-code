1bc2b99a4d65b322784ae635004b8edbb1fc7d20
Shrey Agrawal <shrey.agrawal003@gmail.com>
2023-10-28 23:21:07 +0530

    fixed webpack

--- apps/reader/next.config.js
+++ apps/reader/next.config.js
@@ -76,7 +76,9 @@ module.exports = withSentryConfig(
   base,
   // Make sure adding Sentry options is the last code to run before exporting, to
   // ensure that your source maps include changes from all other Webpack plugins
-  moduleExports,
+  {
+    dryRun: process.env.VERCEL_ENV !== "production"
+  },
   sentryWebpackPluginOptions,
   
 )
