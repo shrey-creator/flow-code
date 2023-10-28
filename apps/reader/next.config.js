faf87736903e6cadcbff11e334c694a81d6b28f5
Shrey Agrawal <shrey.agrawal003@gmail.com>
2023-10-28 23:11:37 +0530

    fixed webpack

--- apps/reader/next.config.js
+++ apps/reader/next.config.js
@@ -16,7 +16,20 @@ const withTM = require('next-transpile-modules')([
 const IS_DEV = process.env.NODE_ENV === 'development'
 const IS_DOCKER = process.env.DOCKER
 
+/**
+ * @type {import('@sentry/nextjs').SentryWebpackPluginOptions}
+ **/
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
 
 /**
  * @type {import('next').NextConfig}
@@ -40,21 +53,30 @@ const config = {
 
 const base = withPWA(withTM(withBundleAnalyzer(config)))
 
-const dev = base
-const docker = base
 
 const moduleExports = {
-  ...config,
-
   sentry: {
     hideSourceMaps: true,
     disableServerWebpackPlugin: false,
     disableClientWebpackPlugin: false,
   },
 };
+const prod = withSentryConfig(
+  base,
+  // Make sure adding Sentry options is the last code to run before exporting, to
+  // ensure that your source maps include changes from all other Webpack plugins
+  moduleExports,
+  sentryWebpackPluginOptions,
+  
+)
 
-const sentryWebpackPluginOptions = {
-  silent: true,
-};
 
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+
+module.exports = withSentryConfig(
+  base,
+  // Make sure adding Sentry options is the last code to run before exporting, to
+  // ensure that your source maps include changes from all other Webpack plugins
+  moduleExports,
+  sentryWebpackPluginOptions,
+  
+)
